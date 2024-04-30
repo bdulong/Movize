644700c0f8e32ca629b2d7ebf0961f4d3fea7d7c
bdulong <baptistedulong@gmail.com>
2024-04-30 10:12:32 +0200

    correction espacement moviedetail

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -3,43 +3,43 @@ import { useParams } from 'react-router-dom';
 import '../App.css';
 
 const MovieDetail = () => {
-  const { id } = useParams();
-  const [movie, setMovie] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const { id } = useParams();
+    const [movie, setMovie] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const apiKey = '4dcc21464991fe06bb4ceb635c4a803b';
-        const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`);
-        const data = await response.json();
-        setMovie(data);
-        setIsLoading(false);
-      } catch (error) {
-        console.error('Erreur lors de la récupération des données : ', error);
-        setError(error);
-        setIsLoading(false);
-      }
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const apiKey = '4dcc21464991fe06bb4ceb635c4a803b';
+                const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`);
+                const data = await response.json();
+                setMovie(data);
+                setIsLoading(false);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des données : ', error);
+                setError(error);
+                setIsLoading(false);
+            }
+        };
 
-    fetchData();
-  }, [id]);
+        fetchData();
+    }, [id]);
 
-  if (isLoading) {
-    return <div>Chargement des données...</div>;
-  }
+    if (isLoading) {
+        return <div>Chargement des données...</div>;
+    }
 
-  if (error) {
-    return <div>Une erreur s'est produite : {error.message}</div>;
-  }
+    if (error) {
+        return <div>Une erreur s'est produite : {error.message}</div>;
+    }
 
-  return (
-    <div>
-      <h1>{movie.title}</h1>
-      <p>{movie.overview}</p>
-    </div>
-  );
+    return (
+        <div>
+        <h1>{movie.title}</h1>
+        <p>{movie.overview}</p>
+        </div>
+    );
 };
 
 export default MovieDetail;

d85ce64a6ad532f4bef500ab41dd4b3899f9f066
bdulong <baptistedulong@gmail.com>
2024-04-30 09:51:29 +0200

    rajout router et page

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const MovieAPIComponent = () => {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+import MovieStart from "./components/MovieStart";
+import MovieDetail from "./components/MovieDetail";
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const apiKey = '4dcc21464991fe06bb4ceb635c4a803b'; // Votre clé API TMDb
-        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
-        const data = await response.json();
-        setMovies(data.results);
-        setIsLoading(false);
-      } catch (error) {
-        console.error('Erreur lors de la récupération des données : ', error);
-        setError(error);
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (isLoading) {
-    return <div>Chargement des données...</div>;
-  }
-
-  if (error) {
-    return <div>Une erreur s'est produite : {error.message}</div>;
-  }
-
+function App() {
   return (
-    <div>
-      <h1>Films populaires</h1>
-      <div className="movies-list">
-        {movies.map(movie => (
-          <div key={movie.id} className="movie-item">
-            {movie.poster_path && (
-              <img
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt={`Affiche de ${movie.title}`}
-              />
-            )}
-            <h2>{movie.title}</h2>
-          </div>
-        ))}
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MovieStart />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
-export default MovieAPIComponent;
+export default App; 
---
+++ src/components/MovieStart.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import '../App.css';
+
+const MovieAPIComponent = () => {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiKey = '4dcc21464991fe06bb4ceb635c4a803b'; // Votre clé API TMDb
+        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
+        const data = await response.json();
+        setMovies(data.results);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données : ', error);
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Chargement des données...</div>;
+  }
+
+  if (error) {
+    return <div>Une erreur s'est produite : {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Films populaires</h1>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <div key={movie.id} className="movie-item">
+            {movie.poster_path && (
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={`Affiche de ${movie.title}`}
+              />
+            )}
+            <h2>{movie.title}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MovieAPIComponent;
